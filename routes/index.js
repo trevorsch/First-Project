8c1746c63706a446c9b2bf8b9e02e99c4e50140c
Trevor Scheuermann <trevorsch1@gmail.com>
2015-06-27 19:46:25 -0600

    changed API key and accidental push to github

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var unirest = require('unirest');
 var mapsKey = process.env.MAPS_API;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // unirest.get('https://www.google.com/maps/embed/v1/search?key=AIzaSyDq_znHjUkfVp2RyUJkwtsO5OF0qp-2gIk&q=Denver');
+  // unirest.get('https://www.google.com/maps/embed/v1/search?key=');
   res.render('index', { title: 'Disc Golf Courses in the Denver Metro Area' });
 });
 
