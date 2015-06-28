5e8604f5d35594b06724c763c3e01124590aa1a0
Trevor Scheuermann <trevorsch1@gmail.com>
2015-06-27 19:25:46 -0600

    added .env, .gitignore, Google Maps API restricted access

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var unirest = require('unirest');
+var mapsKey = process.env.MAPS_API;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // unirest.get('https://www.google.com/maps/embed/v1/search?key=AIzaSyDq_znHjUkfVp2RyUJkwtsO5OF0qp-2gIk&q=Denver');
+  res.render('index', { title: 'Disc Golf Courses in the Denver Metro Area' });
 });
 
 module.exports = router;
