1acc007ca5ccf846f263d099b00ca902e3e78640
Trevor Scheuermann <trevorsch1@gmail.com>
2015-06-29 10:59:21 -0600

    fixed showing API key, added new API key

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var unirest = require('unirest');
-var mapsKey = process.env.MAPS_API;
+var mapsKey = process.env.MAPS_URL;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // unirest.get('https://www.google.com/maps/embed/v1/search?key=');
-  res.render('index', { title: 'Disc Golf Courses in the Denver Metro Area' });
+  res.render('index', { src: mapsKey, title: 'Disc Golf Courses in the Denver Metro Area' });
 });
 
 module.exports = router;
