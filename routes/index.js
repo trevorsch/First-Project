17b523801d50f16b8305b174ea02fee59d495d66
Trevor Scheuermann <trevorsch1@gmail.com>
2015-07-05 22:48:55 -0600

    super hacky adding custom markers using jade only

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var unirest = require('unirest');
 var mapsKey = process.env.MAPS_URL;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var url="https://www.google.com/maps/embed/v1/search?key=AIzaSyCaGK6UNCGfCcwnW8R5zSLmQQyqytXYMWE&q=Denver";
-  res.render('index', { src: url, title: 'Denver Disc Golf' });
+  // var url="https://www.google.com/maps/embed/v1/search?key=AIzaSyCaGK6UNCGfCcwnW8R5zSLmQQyqytXYMWE&q=Denver";
+  res.render('index', { title: 'Denver Disc Golf' });
 });
 
 module.exports = router;
