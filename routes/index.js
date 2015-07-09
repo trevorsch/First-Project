0e1e1f8c471be9cec77dc570bdad9cea5a200f80
Trevor Scheuermann <trevorsch1@gmail.com>
2015-07-08 18:47:23 -0600

    boulder button works

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Denver Disc Golf Courses'});
 });
 
+router.get('/boulder', function(req, res, next) {
+  res.render('boulder', {title: 'Boulder Disc Golf Courses'});
+});
+
 module.exports = router;
