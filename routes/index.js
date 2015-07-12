1f1172da363a971e579060bfc13ef2ab6114c343
Trevor Scheuermann <trevorsch1@gmail.com>
2015-07-12 01:54:51 -0600

    set up route to MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')('localhost/dg-markers');
+var db = require('monk')('localhost/markers');
 var markerCollection = db.get('markers');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Denver Disc Golf Courses'});
+  console.log(markerCollection);
 });
 
 router.get('/boulder', function(req, res, next) {
   res.render('boulder', {title: 'Boulder Disc Golf Courses'});
 });
 
-
+// router.get('/', function(req, res, next){
+//
+// });
 
 module.exports = router;
